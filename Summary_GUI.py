caed52b2ee523d6eb5bbaba7567a8a07ed8f7faf
Gabriel7Pro <gabi@bogdanovsky.com>
2019-05-03 18:46:20 +0300

    Update Summary_GUI.py

--- Summary_GUI.py
+++ Summary_GUI.py
@@ -1,26 +1,32 @@
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.label import Label
+from ss import summary
+import os
 
-
-class WindowFileDropExampleApp(App):
+class WindowFile(App):
     def build(self):
         Window.bind(on_dropfile=self._on_file_drop)
         return
 
+
     def _on_file_drop(self, window, file_path):
-        print(file_path)
+        window.close()
+        Get_all_text(file_path)
         return
 
-class WelcomeScreen(Window):
-	def build(self):
-		return Label(text = "Welcome")
-		
 
-class MyApp(App):
-	def build(self):
-		return WelcomeScreen()
-		
+
+def Get_all_text(directory):
+    texts = []
+
+    for filename in os.listdir(directory):
+        suffix = '.txt'
+        try:
+            f = open(directory + str.encode('\\') + filename)
+            texts.append(f.read())
+        except Exception as e:
+            pass
+    summary(texts)
 
 if __name__ == '__main__':
-    MyApp().run()
+    WindowFile().run()
